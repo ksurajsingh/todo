90124af46e206fed81abec2cc0265202c4a54525
ksurajsingh <surajsinghk013@gmail.com>
2026-03-12 07:57:27 +0530

    chore(gitignore):mod

--- be/src/main/java/kss/utility/todo/entity/todo.java
+++ be/src/main/java/kss/utility/todo/entity/todo.java
@@ -1,16 +1,18 @@
 package kss.utility.todo.entity;
 
 import jakarta.persistence.*;
+import kss.utility.todo.entity.category;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name="todo")
-public class todo {
+public class Todo {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
@@ -21,10 +23,20 @@ public class todo {
     private String description;
     private String status="todo";
 
+    @ManyToMany
+    @JoinTable(
+            name="todo_category",
+            joinColumns = @JoinColumn(name = "todo_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private List<category> categories;
+
 
     @CreationTimestamp
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 }
