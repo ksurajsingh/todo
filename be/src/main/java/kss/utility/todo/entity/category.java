035c6fe045cc6988f37b924fc834c28a3e79a7f8
ksurajsingh <surajsinghk013@gmail.com>
2026-03-13 07:58:54 +0530

    be(entity): add category

---
+++ be/src/main/java/kss/utility/todo/entity/category.java
@@ -0,0 +1,18 @@
+package kss.utility.todo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="category")
+public class category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+}
