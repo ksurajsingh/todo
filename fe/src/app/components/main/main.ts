a805a75f2e5e0071c23644240f6cb1f89fe5af3e
ksurajsingh <surajsinghk013@gmail.com>
2026-03-18 08:08:25 +0530

    fe(main): use timer to get todos

--- fe/src/app/components/main/main.ts
+++ fe/src/app/components/main/main.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { todo } from '../../models/todo.models';
 import { todoService } from '../../services/todo.service';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef } from '@angular/core';
-import { interval, switchMap, Subject, takeUntil } from 'rxjs';
+import { switchMap, Subject, takeUntil, timer, tap } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -22,13 +21,23 @@ export class Main implements OnInit{
   ){}
 
   ngOnInit(): void {
-    interval(5000)
+    timer(0,5000)
     .pipe(
+      tap(()=>console.log("pokemane")),
       takeUntil(this.destroy$),
-      switchMap(()=>this.todoService.getTodos()))
+      switchMap(()=>this.todoService.getTodos()),
+      tap(()=>console.log("test")),
+      tap(data => console.log("first item", data[0])))
     .subscribe(data=>{
+      console.log("2")
       this.todos=data;
       this.cdr.detectChanges();
     })
   }
+
+  ngOnDestroy():void{
+    this.destroy$.next(),
+    this.destroy$.complete()
+  }
+
 }
