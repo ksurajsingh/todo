6234c0c2b0c74bb9930e5ce68482da866992ae20
ksurajsingh <surajsinghk013@gmail.com>
2026-05-10 07:58:15 +0530

    be(entity):mod todo

--- be/src/main/java/kss/utility/todo/entity/todo.java
+++ be/src/main/java/kss/utility/todo/entity/todo.java
@@ -1,7 +1,6 @@
 package kss.utility.todo.entity;
 
 import jakarta.persistence.*;
-import kss.utility.todo.entity.category;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -12,7 +11,7 @@ import java.util.List;
 @Entity
 @Data
 @Table(name="todo")
-public class Todo {
+public class todo {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
